5880d396c3b04c0e0e27ea5b9e14aa28e05e4d00
Tom Barrett <tcbarrett@gmail.com>
2013-09-14 16:13:31 +0100

    Initial commit

---
+++ where-where.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Where & When
+ * Plugin URI: https://github.com/tcbarrett/when-and-where
+ * Description: Note a weekly list of where and when for your business (e.g. pilates classes)
+ * Author: TCBarrett
+ * Version: 0.1
+ * Author URI: http://www.tcbarrett.com/
+ * Text Domain: where-and-when
+ * Domain Path: /lang/
+ */
+define( 'WPG_TEXTDOMAIN', 'where-and-when' );
